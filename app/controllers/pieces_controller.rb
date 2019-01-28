807da8c8cddcfdd9f91ed43ff7b715317b547008
Patrick Rodriguez <pat.rodriguez108@gmail.com>
2019-01-28 09:11:39 -0600

    Set up AWS

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,5 +4,26 @@ class PiecesController < ApplicationController
   end
 
   def new
+    @piece = Piece.new
+  end
+
+  def create
+    @piece = Piece.new(piece_params)
+
+    if @piece.save
+      redirect_to "/pieces/#{@piece.id}"
+    else
+      render action: 'new'
+    end
+  end
+
+  def show
+    @piece = Piece.find_by(id: params[:id])
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:name, :description, :img)
   end
 end
